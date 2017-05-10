b443ebe8c5c7f643f2db4158f9cb9473d1721447
Peraphol <peraphol.b@gmail.com>
2017-05-10 23:14:54 +0700

    change activity add local resource

--- app/src/main/java/com/jukgu/startandroid/MainActivity.java
+++ app/src/main/java/com/jukgu/startandroid/MainActivity.java
@@ -1,15 +1,28 @@
 package com.jukgu.startandroid;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.List;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int PICK_CONTACT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +44,65 @@ public class MainActivity extends AppCompatActivity {
 //        Intent callIntent = new Intent(Intent.ACTION_VIEW, uri);
 
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
         emailIntent.setType("text/plain");
         emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"peraphol.b@gmail.com"});
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email Subject");
         emailIntent.putExtra(Intent.EXTRA_TEXT, "Email massage text.");
         emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
 
-        startActivity(emailIntent);
+//        Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+//        Calendar beginTime = Calendar.getInstance();
+//        beginTime.set(2017, 4, 9, 7, 30);
+//        Calendar endTime = Calendar.getInstance();
+//        endTime.set(2017, 4, 9, 10, 30);
+//        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+//        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+//        calendarIntent.putExtra(CalendarContract.Events.TITLE, "Ninja class");
+//        calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret dojo");
+
+//        PackageManager packageManager = getPackageManager();
+//        List<ResolveInfo> activities = packageManager.queryIntentActivities(emailIntent, 0);
+//        boolean isIntentSafe = activities.size() > 0;
+//
+//        if(isIntentSafe) {
+//            startActivity(emailIntent);
+//        }
+
+        String title = getResources().getString(R.string.chooser_title);
+        Intent chooser = Intent.createChooser(emailIntent, title);
+
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
+    }
+
+    public void goAndGetResult(View view) {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                Uri contactUri = data.getData();
+
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+
+                Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                int colName = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                String number = cursor.getString(column);
+                String name = cursor.getString(colName);
+
+                TextView showReultText = (TextView) findViewById(R.id.showResult);
+                showReultText.setText(name + " : " + number);
+            }
+        }
     }
 }
